b723bb38d0fab65f885c69f14dc4613541e37ad0
owner <canerduru@ymail.com>
2021-01-02 17:40:38 +0300

    test

--- GoogleAdMobTest/ViewController.swift
+++ GoogleAdMobTest/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import GoogleMobileAds
 
 class ViewController: UIViewController {
+    //2
+    var interstitial: GADInterstitial!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        //3 bu id degisecek
+        interstitial = GADInterstitial(adUnitID: "ca-app-pub-3940256099942544/4411468910")
+
+        //4 reklami yukle ama hala gostermedi
+        let request = GADRequest()
+            interstitial.load(request)
+        
     }
 
-
+    @IBAction func nextClicked(_ sender: Any) {
+        //5 reklami gosterme
+        if interstitial.isReady {
+           interstitial.present(fromRootViewController: self)
+         }
+    }
+    
 }
 
